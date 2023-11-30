2c561fba17c53ca30c5bc3994233b11dfa5cd30f
Hồng Yến <63199543+nguyenhongyen@users.noreply.github.com>
2023-11-30 23:47:22 +0700

    update code4

--- assets/js/style.js
+++ assets/js/style.js
@@ -23,10 +23,11 @@
                     speed: 1000,
                     effect:'fade',
                     slidesPerView: 1,
-                   /* autoplay: {
+                    loop:true,
+                    autoplay: {
                         delay: 5500,
                         disableOnInteraction:true,
-                    },*/
+                    },
                     pagination: {
                         el: "#section-hero .swiper-pagination",
                         clickable: true,
@@ -55,11 +56,76 @@
             })
         }
 
+        const handleNavbar = function(){
+            if($('#menu-bars').length){
+                $('#menu-bars').click(function(){
+                    $('body').removeClass('show-search').addClass('is-show show-navbar');
+                });
+            }
+
+            if($('#close-menu').length){
+                $('#close-menu').click(function(){
+                    $('body').removeClass('is-show show-navbar');
+                });
+            }
+
+        }
+        const handleSearchMobile = function(){
+            if($('#call-search').length){
+                $('#call-search').click(function(){
+                    $('body').removeClass('is-show show-navbar');
+                    $('body').toggleClass('show-search');
+                    $(this).toggleClass('is-search');
+
+                });
+            }
+        }
+
+        const handleSliderPartner = function () {
+            if ($('#section-partner').length) {
+                new Swiper('#section-partner', {
+                    speed: 1000,
+                    slidesPerView: 1.3,
+                    spaceBetween: 30,
+                    loop:true,
+                    autoplay: {
+                        delay: 3500,
+                        disableOnInteraction:true,
+                    },
+                    breakpoints: {
+                        320: {
+                            slidesPerView: 1.5,
+                        },
+                        425: {
+                            slidesPerView: 2,
+                        },
+                        576: {
+                            slidesPerView: 2.5,
+                        },
+                        768: {
+                            slidesPerView: 3.5
+                        },
+                        992: {
+                            slidesPerView: 4.5
+                        },
+                        1200: {
+                            slidesPerView: 6,
+                        },
+
+                    }
+                });
+            }
+        }
+
         $(function () {
             handleSliderBlog();
             handleSliderHero();
             handleHeightCategory();
             handleSearchProduct();
+            handleNavbar();
+            handleSearchMobile();
+            handleSliderPartner();
+
         });
     })(jQuery);
 
