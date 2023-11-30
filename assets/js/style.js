c8a60e049447a185a8eac80b4458149d657d1619
Vietcore-Yen <hongyen.27nguyen@gmail.com>
2023-11-30 16:01:54 +0700

    update new

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,25 +3,41 @@
         'use strict';
 
 
+        const handleSliderBlog= function () {
+            if ($('#header-slider').length) {
+                new Swiper('#header-slider', {
+                    speed: 1000,
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+                    autoplay: {
+                        delay: 3500,
+                        disableOnInteraction:true,
+                    },
+                });
+            }
+        }
+
         const handleSliderHero= function () {
             if ($('#section-hero').length) {
                 new Swiper('#section-hero', {
                     speed: 1000,
                     effect:'fade',
                     slidesPerView: 1,
+                   /* autoplay: {
+                        delay: 5500,
+                        disableOnInteraction:true,
+                    },*/
                     pagination: {
-                        el: ".swiper-pagination",
+                        el: "#section-hero .swiper-pagination",
                         clickable: true,
                     },
                     navigation: {
-                        nextEl: ".button-service_next",
-                        prevEl: ".button-service_prev",
+                        nextEl: "#section-hero .button-prev",
+                        prevEl: "#section-hero .button-next",
                     },
                 });
             }
         }
-
-
 
         const handleHeightCategory = function(){
             if ($('#navigation-category').length) {
@@ -32,10 +48,18 @@
             }
         }
 
-        $(function () {
+        $('#result-search').fadeOut();
+        const handleSearchProduct = function(){
+            $('#frm-search').submit(function (){
+                $('#result-search').fadeIn();
+            })
+        }
 
+        $(function () {
+            handleSliderBlog();
             handleSliderHero();
             handleHeightCategory();
+            handleSearchProduct();
         });
     })(jQuery);
 
